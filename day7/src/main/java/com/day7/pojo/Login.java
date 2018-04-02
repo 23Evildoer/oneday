696faba7adc0f4f55ca9915c13445959c5bc950b
jiaoyeye <875804969@qq.com>
2018-04-02 16:03:35 +0800

    创建 Hotel 类

---
+++ day7/src/main/java/com/day7/pojo/Login.java
@@ -0,0 +1,25 @@
+package com.day7.pojo;
+
+/**
+ * @s
+ */
+public class Login {
+    private  Integer age;
+    private  String  name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
